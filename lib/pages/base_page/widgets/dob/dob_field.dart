2638bbc160b5c9c62cfcd54824e0b0bf007bd6a2
rohit0110 <rohit7rathore15@gmail.com>
2022-06-08 16:50:21 +0530

    Add name input containers

---
+++ lib/pages/base_page/widgets/dob/dob_field.dart
@@ -0,0 +1,69 @@
+import '../../../../utils/constants.dart';
+import 'package:flutter/material.dart';
+
+class DOBContainer extends StatefulWidget {
+  const DOBContainer({Key? key, required this.callback}) : super(key: key);
+  final StringCallback callback;
+  @override
+  State<DOBContainer> createState() => _DOBContainerState();
+}
+
+class _DOBContainerState extends State<DOBContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.transparent,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          const Text(
+            "give us your DOB",
+            style: headingStyle,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Text(
+            "additional information helps us fetch",
+            style: subheadingStyle,
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          const Text(
+            "accurate credit reports",
+            style: subheadingStyle,
+          ),
+          const SizedBox(height: 20),
+          TextField(
+            onChanged: (value) {
+              setState(() {
+                widget.callback(value);
+              });
+            },
+            maxLength: 10,
+            cursorHeight: 30,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+            decoration: const InputDecoration(
+              counterText: "",
+              enabledBorder: InputBorder.none,
+              focusedBorder: InputBorder.none,
+              hintText: "dd/mm/yyyy",
+              hintStyle: TextStyle(
+                color: inputColor,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            keyboardType: TextInputType.datetime,
+          ),
+        ],
+      ),
+    );
+  }
+}
